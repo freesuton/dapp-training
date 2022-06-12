ffb7dc60abece8b151666c3af5d9f5d6a06c7d6f
slowpon <slowpon@outlook.com>
2022-06-12 11:45:41 -0400

    iterable mapping

--- contracts/merkleTree.sol
+++ contracts/merkleTree.sol
@@ -1,22 +1,63 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
+contract MerkleProof {
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf,
+        uint index
+    ) public pure returns (bool){
+        bytes32 hash = leaf;
 
-contract Greeter {
-    string private greeting;
+        for(uint i = 0; i < proof.length; i++){
+            bytes32 proofElement = proof[i];
+            if(index % 2 == 0){
+                hash = keccak256(abi.encodePacked(hash, proofElement));
+            }else {
+                hash = keccak256(abi.encodePacked(proofElement, hash));
+            }
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+            index = index / 2;
+        }
+
+        return hash == root;
+    }
+}
+
+contract TestMerkleProof is MerkleProof {
+    bytes32[] public hashes;
+
+    constructor() {
+        string[4] memory transactions = [
+            "alice -> bob",
+            "bob -> dave",
+            "carol -> alice",
+            "dave -> bob"
+        ];
+
+        for(uint i = 0; i < transactions.length; i++){
+            hashes.push(keccak256(abi.encodePacked(transactions[i])));
+        }
+
+        uint n = transactions.length;
+        uint offset = 0;
+
+        while( n > 0){
+            for(uint i = 0; i < n - 1; i += 2){
+                hashes.push(
+                    keccak256(
+                        abi.encodePacked(hashes[offset + i], hashes[offset + i +1])
+                    )
+                );
+            }
+            offset += n;
+            n = n /2;
+        }
+
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function getRoot() public view returns (bytes32){
+        return hashes[hashes.length - 1];
     }
 }
